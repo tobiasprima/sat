b7fbaeac0377020bd01685c36bb91c76e0d7f851
Surya Saputra <17169567+suryasaputra2016@users.noreply.github.com>
2024-11-19 21:25:13 +0700

    feat: add go.mod and username and password prompt in main.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module SAT
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Input user name:")
+	var username string
+	fmt.Scan(&username)
+
+	fmt.Print("Input password:")
+	var password string
+	fmt.Scan(&password)
+
+}
