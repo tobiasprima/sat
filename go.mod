0820e16c27ca69339ea48fa8367c74d34b3a3ad0
Surya Saputra <17169567+suryasaputra2016@users.noreply.github.com>
2024-11-20 21:43:54 +0700

    feat: add database connection to supabase

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module SAT
 
 go 1.23.2
+
+require github.com/lib/pq v1.10.9 // indirect
--- main.go
+++ main.go
@@ -1,8 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
 
 func main() {
+	db_name := "postgres"
+	db_user := "postgres.wyxrurmpypoqythiftvq"
+	db_pass := "PSiRcsVEJ9NsTjv8"
+	db_host := "aws-0-ap-southeast-1.pooler.supabase.com"
+
+	db, err := sql.Open("postgres", fmt.Sprintf("dbname=%s user=%s password=%s host=%s sslmode=require", db_name, db_user, db_pass, db_host))
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+
 	fmt.Print("Input user name:")
 	var username string
 	fmt.Scan(&username)
