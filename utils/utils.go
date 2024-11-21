bfc6ce4007d8e3011217918e04e79bed1dec7a70
Tobias Prima <primatjahja@gmail.com>
2024-11-21 21:56:37 +0800

    feat: capitalize name function

---
+++ utils/utils.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"strings"
+)
+
+
+func CapitalizeName(name string) string {
+	words := strings.Fields(name)
+	for i, word := range words {
+		if len(word) > 0 {
+			words[i] = strings.ToUpper(string(word[0])) + strings.ToLower(word[1:])
+		}
+	}
+	return strings.Join(words, " ")
+}
